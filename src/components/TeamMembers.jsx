0549f6c6c45985a0b29eaa39bc421f348539ddc3
Tiago Morgado <Tiago.alcm@icloud.com>
2023-05-25 22:13:15 +0200

    code cleanup

--- src/components/TeamMembers.jsx
+++ src/components/TeamMembers.jsx
@@ -3,7 +3,7 @@ import TeamMemberCard from "./TeamMemberCard"
 const TeamMembers = ({employees, selectedTeam, handleEmployeeCardClick}) => {
     return (
         employees.map((employee) => (
-            <TeamMemberCard employee={employee} selectedTeam={selectedTeam} handleEmployeeCardClick={handleEmployeeCardClick}/>
+            <TeamMemberCard key={employee.id} employee={employee} selectedTeam={selectedTeam} handleEmployeeCardClick={handleEmployeeCardClick}/>
         )) 
     )
 }
